d664d586557357b6efff5db99094ada725b110a3
Sadiq Okocha <sokocha@gmail.com>
2017-09-26 09:51:22 +0000

    adds catdog file for teaching tomisin

---
+++ catdog.py
@@ -0,0 +1,13 @@
+class Cat():
+  def talk(self):
+    print ("Meow")
+
+class Dog():
+  def talk(self):
+    print("Woof")
+
+# mr_bubbles = Cat()
+# mr_bubbles.talk()
+
+spike = Dog()
+spike.talk()
