328d79d3d16ccf438e7db7e98326b0824154730a
Sadiq Okocha <sokocha@gmail.com>
2017-09-26 11:10:41 +0000

    imports hash module

--- encapsulation.py
+++ encapsulation.py
@@ -1,11 +1,31 @@
+import hashlib, binascii
+
 class Secret():
   def __init__(self,secret,password):
     self.__secret = secret
     self.__password = password
 
+
+
   def get_secret(self,password):
     if password == self.__password:
-      return self.__secret
+      print (self.__secret)
+      # return self.__secret
+      choice = input("would you like to change the password? ")
+
+      if choice == "yes":
+        new_password = input("What would you like to set password to? ")
+        self.__password = new_password
+        print(self.__dict__)
+
+      else:
+        return
 
 andrew_secret = Secret("I am beautiful","truth")
 andrew_secret.get_secret("truth")
+
+
+# 1. Create a method that lets you change password given the current password
+
+
+# 2. Hash the password when a secret is created
